d2d29cf1817270eb8d6f59d3d4277e0388da0445
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2024-11-10 23:25:48 +0530

    Project setup with reactRouter and install firebase and tailwind and daisiUi

---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB6VlmfO4dIm-xCgiIvT-XJqzZdRyZ9oXU",
+  authDomain: "auth-moha-milon-f84ca.firebaseapp.com",
+  projectId: "auth-moha-milon-f84ca",
+  storageBucket: "auth-moha-milon-f84ca.firebasestorage.app",
+  messagingSenderId: "409952869327",
+  appId: "1:409952869327:web:c98e04b2b2a5bc4a7d10ba"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
